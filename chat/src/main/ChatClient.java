478a87b8837144585032fb909039f2c0df64d8e2
Simon Barrameda <barramedasimon321@gmail.com>
2016-10-25 22:26:06 +0800

    Message passing between 1 server and 1 client

--- chat/src/main/ChatClient.java
+++ chat/src/main/ChatClient.java
@@ -1,14 +1,42 @@
 package src.main;
 
-import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
 import src.config.Config;
 
 public class ChatClient {
+    // private Socket socket;
+    //
+    // public ChatClient() {
+    //     socket = new Socket(Config.SERVER_NAME, Config.PORT);
+    // }
 
     public static void main(String[] args) throws IOException{
         Socket socket = new Socket(Config.SERVER_NAME, Config.PORT);
-        
+
+        OutputStream outToServer = socket.getOutputStream();
+        DataOutputStream out = new DataOutputStream(outToServer);
+
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
+
+        InputStream inFromServer = socket.getInputStream();
+        DataInputStream in = new DataInputStream(inFromServer);
+
+        System.out.println("The client started. To quit it type 'Ok'.");
+        String response;
+        while(true) {
+            System.out.print("Say: ");
+            out.writeUTF(br.readLine());
+
+            System.out.println(in.readUTF());
+        }
     }
 
 }
--- chat/src/main/ChatServer.java
+++ chat/src/main/ChatServer.java
@@ -1,22 +1,49 @@
 package src.main;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.lang.Thread;
 import java.net.ServerSocket;
-import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
 import src.config.Config;
 
-public class ChatServer extends Thread {
+public class ChatServer implements Runnable {
     private ServerSocket serverSocket;
 
     public ChatServer() throws IOException {
         serverSocket = new ServerSocket(Config.PORT);
         serverSocket.setSoTimeout(0);
     }
-    public void run() {
-        boolean connected = true;
-    }
-    public static void main(String[] args) throws IOException {
 
+    //@Override
+    public void run() {
+        ArrayList<ChatClient> clients = new ArrayList<ChatClient>();
+        boolean connected = true;
+
+        try {
+            String msg = "info: Server is running on port "
+                        + serverSocket.getLocalPort() + " .. ";
+
+            System.out.println(msg);
+
+            Socket server = serverSocket.accept();
+            DataInputStream in = new DataInputStream(server.getInputStream());
+            DataOutputStream out = new DataOutputStream(server.getOutputStream());
+            while(!server.isClosed()) {
+                msg = in.readUTF();
+                out.writeUTF("Server says: " + msg);
+            }
+        } catch (IOException e) {
+            String err = "Accept failed on: " + serverSocket.getLocalPort();
+            System.out.println(err);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Thread server = new Thread(new ChatServer());
+        server.start();
     }
 
 }
