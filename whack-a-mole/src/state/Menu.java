4faa2420f7dbdc358c7886220656b6567eaee78d
cbbobilles <bobillescoleen@gmail.com>
2016-11-14 16:27:02 +0800

    updated Menu state

--- whack-a-mole/src/state/Menu.java
+++ whack-a-mole/src/state/Menu.java
@@ -3,10 +3,12 @@ package state;
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.Animation;
 
 import config.GameConfig;
 import main.LobbyClient;
@@ -15,27 +17,30 @@ import main.LobbyServer;
 public class Menu extends BasicGameState {
 	
 	private String mouse;
+	private Image choices;
+	private Animation titleAnimation;
 
 	public Menu() {
 		this.mouse = "No Input Yet.";
 	}
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		
+		Image[] menu = {new Image("res/whack-a-mole-3.png"), 
+				new Image("res/whack-a-mole-4.png")
+			};
+		choices = new Image("res/mana-full-2.png");
+		titleAnimation = new Animation(menu, 100);
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-		g.drawString("MAIN MENU", 50, 50);
-		g.drawString("Play", 60, 75);
-		g.drawString("Create Lobby", 60, 105);
-		g.drawString("Join Lobby", 60, 135);
-		g.drawString("Exit", 60, 165);
+		titleAnimation.draw(-20,30);
+		choices.draw(360, 280, 300, 250);
 		g.drawString(mouse, 10, 580);
+		
 
-		g.drawRect(55, 70, 50, 25);
-		g.drawRect(55, 100, 118, 25);
-		g.drawRect(55, 130, 100, 25);
-		g.drawRect(55, 160, 50, 25);
+		g.drawRect(340, 275, 340, 70);
+		g.drawRect(340, 360, 340, 70);
+		g.drawRect(340, 440, 340, 70);
 	}
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
@@ -44,14 +49,7 @@ public class Menu extends BasicGameState {
 		int ypos = Mouse.getY();
 		mouse = "Mouse at x: " + xpos + " y: " + ypos;
 		
-		if((xpos>55 && xpos<105) && (ypos>503 && ypos<528)) {
-			if(input.isMouseButtonDown(0)) {
-				sbg.addState(new Play());
-				sbg.enterState(GameConfig.PLAY);
-			}
-		}
-		
-		if((xpos>55 && xpos<173) && (ypos>473 && ypos<498)) {
+		if((xpos>340 && xpos<680) && (ypos>250 && ypos<320)) { //Create lobby
 			if(input.isMouseButtonDown(0)) {
 				sbg.addState(new Lobby("host"));
 				sbg.enterState(GameConfig.LOBBY);
@@ -59,7 +57,7 @@ public class Menu extends BasicGameState {
 			}
 		}
 		
-		if((xpos>55 && xpos<155) && (ypos>443 && ypos<468)) {
+		if((xpos>340 && xpos<680) && (ypos>170 && ypos<240)) { //Join lobby
 			if(input.isMouseButtonDown(0)) {
 				sbg.addState(new Lobby("client"));
 				sbg.enterState(GameConfig.LOBBY);
@@ -67,7 +65,7 @@ public class Menu extends BasicGameState {
 			}
 		}
 		
-		if((xpos>55 && xpos<105) && (ypos>413 && ypos<438)) {
+		if((xpos>340 && xpos<680) && (ypos>90 && ypos<160)) { //Exit
 			if(input.isMouseButtonDown(0)) {
 				System.exit(0);
 			}
